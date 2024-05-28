c3f7d61ba0fc992acfad25e8808e5ac59758a341
sushantnair <sushantnair.developer@gmail.com>
2024-05-28 17:48:33 +0530

    Create ArrayBasic.cpp

---
+++ PAN050338-TUE280524/ArrayBasic.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+    // Declare an integer array named 'numbers' with a size of 5 and initialize
+    int numbers[5] = {1, 2, 3, 4, 5};
+
+    // Output the third element (index 2) of the array
+    std::cout << "Third element: " << numbers[2] << std::endl;
+
+    return 0;
+}
