28882e55da06413bf4092b47c78837da261fc935
sushantnair <sushantnair.developer@gmail.com>
2024-05-28 18:01:20 +0530

    Create StringsBasic.cpp

---
+++ PAN050338-TUE280524/StringsBasic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+
+int main(){
+    std::string greeting = "Hare Krishna, ";
+    std::string name = "Govinda.";
+    std::cout << greeting + name << std::endl;
+
+    return 0;
+}
